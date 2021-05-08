ae94b058960e70e8476cbb92dd64561e03df0282
Ryan-0207 <f20200350@pilani.bits-pilani.ac.in>
2021-05-08 18:47:52 +0530

    vaccinejson

---
+++ lib/vaccinecentre.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class VaccineCentre extends StatefulWidget {
+  final String pincode;
+  VaccineCentre({this.pincode});
+  @override
+  _VaccineCentreState createState() => _VaccineCentreState();
+}
+
+class _VaccineCentreState extends State<VaccineCentre> {
+  var finalDate = '';
+  getCurrentDate() {
+    var date = new DateTime.now().toString();
+
+    var dateParse = DateTime.parse(date);
+
+    var formattedDate = "${dateParse.day}-${dateParse.month}-${dateParse.year}";
+
+    setState(() {
+      finalDate = formattedDate.toString();
+    });
+  }
+
+  Map vaccinedata;
+  getdata(String pin, String date) async {
+    final inputdata = {'pincode': pin, 'date': finalDate};
+    final uri = Uri.https('https://cdn-api.co-vin.in',
+        '/api/v2/appointment/sessions/public/findByPin', inputdata);
+
+    http.Response response = await http.get(uri);
+    setState(() {
+      vaccinedata = json.decode(response.body);
+    });
+  }
+
+  @override
+  void initState() async {
+    getCurrentDate();
+    await getdata(widget.pincode, finalDate);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
